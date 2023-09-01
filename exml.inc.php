424e87cfe31c29a6a1c661eb59133c00bd588a40
Daniel Silva Ferreira <danielsilva@usp.br>
2023-09-01 09:02:49 -0300

    modelos e //explicações principais

--- exml.inc.php
+++ exml.inc.php
@@ -118,6 +118,9 @@ class exml extends ImportExportPlugin2
         return 'exml';
     }
 
+    /**
+     * FUNÇÃO PRINCIPAL, RESPOSÁVEL PELA ESTRUTURA DO ARQUIVO XML.
+     */
     public function exportSubmissions($submissionIds, $context, $user, $request)
     {
         $submissionDao = DAORegistry::getDAO('SubmissionDAO'); /* @var $submissionDao SubmissionDAO */
@@ -125,17 +128,17 @@ class exml extends ImportExportPlugin2
         $app = new Application();
         $request = $app->getRequest();
         $press = $request->getContext();
+
+        /********************************************		FOREACH'S	********************************************/
         foreach ($submissionIds as $submissionId) {
             $submission = $submissionDao->getById($submissionId, $context->getId());
             if ($submission) {
                 $submissions[] = $submission;
             }
         }
-
-        /********************************************		FOREACH'S	********************************************/
-
         $authorsInfo = [];
         $authors = $submission->getAuthors();
+
         foreach ($authors as $author) {
             $authorInfo = [
         'givenName' => $author->getLocalizedGivenName(),
@@ -149,11 +152,11 @@ class exml extends ImportExportPlugin2
         foreach ($submissions as $submission) {
             // Obtendo o título da submissão
             $submissionTitle = $submission->getLocalizedFullTitle();
-            //obtendo o tipo de conteudo, capitulo e monografia
-            $types = [1 => 'chapter', 2 => 'monograph'];
+            //obtendo o tipo de conteudo, capitulo e monografia. crossref só aceita "edited_book, monograph, reference, other" porém ao iniciar uma nova publicação, só há entrada para 'monograph' e 'other'
+            $types = [1 => 'other', 2 => 'monograph', 3 => 'other', 4 => 'other'];
             $type = $submission->getWorkType();
 
-            //$xablau = $submission->getStoredPubId('isbn');
+            //$xablau = $submission->getStoredPubId('isbn'); //teste para captura de isbn
 
             $abstract = $submission->getLocalizedAbstract();
             $doi = $submission->getStoredPubId('doi');
@@ -167,13 +170,12 @@ class exml extends ImportExportPlugin2
             //timestamp
             $timestamp = date('YmdHis').substr((string) microtime(), 2, 3);
 
-            // aqui retorna xx_XX$submissionLanguage = $submission->getLocale();
-    $submissionLanguage = substr($submission->getLocale(), 0, 2); //aqui retorna xx
-    $publisherName = $press->getData('publisher');
+            // aqui retorna xx_XX (pt-BR ou en_US etc) sendo o idioma em que a publicação foi submetida
+            $submissionLanguage = substr($submission->getLocale(), 0, 2); //aqui retorna xx
+            $publisherName = $press->getData('publisher');
             $registrant = $press->getLocalizedName();
-            //$editionNumber = $submission->getSeriesPosition();
 
-            // Obtendo dados do autor
+            // Obtendo dados dos autores
             $authorNames = [];
             $authors = $submission->getAuthors();
             foreach ($authors as $author) {
@@ -185,23 +187,27 @@ class exml extends ImportExportPlugin2
             $authorName = implode(', ', $authorNames);
             $orcid = $author->getOrcid();
 
-            /********************************************		ESTRUTURA XML		********************************************/
-            //---início estrutura xml
+            /*
+             *
+             * ESTRUTURA XML
+             *
+             * */
 
+            //---início estrutura xml codigos obrigatórios
             $xmlContent = '<?xml version="1.0" encoding="UTF-8"?>
 		<doi_batch version="4.4.2" xmlns="http://www.crossref.org/schema/4.4.2" 
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
 		xmlns:jats="http://www.ncbi.nlm.nih.gov/JATS1" 
 		xsi:schemaLocation="http://www.crossref.org/schema/4.4.2 http://www.crossref.org/schema/deposit/crossref4.4.2.xsd">';
 
-            //$xmlContent .= '<TESTE>'.$xablau.'</TESTE>';
+            //$xmlContent .= '<TESTE>'.$xablau.'</TESTE>';//tag para testes
 
             $xmlContent .= '<head>';
             //segundo documentação, doi_batch_id pode ser o proprio nome da publicação: https://www.crossref.org/documentation/register-maintain-records/verify-your-registration/submission-queue-and-log/
-            //$xmlContent .= '<doi_batch_id>ba60f6118992d8a5a2-5a37</doi_batch_id>';
             $xmlContent .= '<doi_batch_id>'.htmlspecialchars($submissionTitle).'</doi_batch_id>';
             $xmlContent .= '<timestamp>'.$timestamp.'</timestamp>';
             $xmlContent .= '<depositor>';
+            //por hora em hardcoding - buscando solução para obter info via form
             $xmlContent .= '<depositor_name>sibi:sibi</depositor_name> ';
             $xmlContent .= '<email_address>dgcd@abcd.usp.br</email_address>';
             $xmlContent .= '</depositor>';
@@ -215,7 +221,7 @@ class exml extends ImportExportPlugin2
             $xmlContent .= '<contributors>';
 
             //AUTORES:
-            // Primeiro autor
+            // Primeiro autor - obrigatório
             $firstAuthor = reset($authorsInfo);
             if (!empty($authorInfo['afiliation'])) {
                 $xmlContent .= '<organization sequence="additional" contributor_role="author">'.htmlspecialchars($authorInfo['afiliation']).'</organization>';
@@ -244,14 +250,13 @@ class exml extends ImportExportPlugin2
             }
             $xmlContent .= '</contributors>';
 
+            //dados do livro
             $xmlContent .= '<titles>';
             $xmlContent .= '<title>'.htmlspecialchars($submissionTitle).'</title>';
             $xmlContent .= '</titles>';
-
             $xmlContent .= '<jats:abstract xml:lang="'.htmlspecialchars($submissionLanguage).'">';
             $xmlContent .= '<jats:p>'.htmlspecialchars($abstract).'</jats:p>';
             $xmlContent .= '</jats:abstract>';
-
             $xmlContent .= '<publication_date media_type="online">';
             $xmlContent .= '<month>'.htmlspecialchars($publicationMonth).'</month>';
             $xmlContent .= '<day>'.htmlspecialchars($publicationDay).'</day>';
@@ -259,28 +264,30 @@ class exml extends ImportExportPlugin2
             $xmlContent .= '</publication_date>';
 
             //$isbn = $submission->getLocalizedISBN();
-            //falta essa parte
+            //falta essa parte - problema relatado, em investigação.
             $isbn = '997-65-43210-12-3';
             $xmlContent .= '<isbn>'.htmlspecialchars($isbn).'</isbn>';
 
             $xmlContent .= '<publisher>';
-            //copyright
+            //como no modelo, publisher é o detentor do copyright
             $xmlContent .= '<publisher_name>'.htmlspecialchars($copyright).'</publisher_name>';
             $xmlContent .= '</publisher>';
-
             $xmlContent .= '<doi_data>';
             $xmlContent .= '<doi>'.htmlspecialchars($doi).'</doi>';
             $xmlContent .= '<resource>'.htmlspecialchars($publicationUrl).'</resource>';
             $xmlContent .= '</doi_data>';
             $xmlContent .= '</book_metadata>';
             $xmlContent .= '</book>';
-
             $xmlContent .= '</body>';
             $xmlContent .= '</doi_batch>';
         }
 
         return $xmlContent;
     }
+
+    /**
+     * Final estrutura XML.
+     */
 
     /**
      * @copydoc ImportExportPlugin::executeCLI
